bcb52eea8d5150f2c6483f15f6f4c0c6ab3ae61e
Aramoro <35650608+Yogo-Isoroku@users.noreply.github.com>
2019-10-09 00:59:12 -0700

    changed index.js console.log to return

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ module.exports = str => {
 	let random = getRandom(str.crystals.length);
 	str.crystals.map((x, i) => {
 		if (random === i) {
-			console.log(x.name);
+			return x.name;
 		}
 	});
 };
